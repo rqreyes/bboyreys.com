3c25f1a3d467af50eae463a935ca9a1116b3c40a
Randy Reyes <rqreyes@gmail.com>
2017-03-20 20:40:59 -0700

    Sync with live

--- 404.php
+++ 404.php
@@ -1,24 +1,24 @@
 <?php
 	$pageTitle = 'B-Boy Rey-S | 404 Error';
-	$robots = '<meta name="robots" content="noindex, nofollow">';
+	$robots = "<meta name='robots' content='noindex, nofollow'>";
 	$htmlClasses = 'contact extra';
 	include 'includes/header.php';
-	include 'includes/nav-desktop.php' ?>
+?>
 
-	<section class='container-contact'>
-		<header class='mobile'>
-			<img class='logo' src='images/logo.png' width='60' height='auto'>
-			<h1>B-boy Rey-S</h1>
-		</header>
+		<section class='container-contact'>
+			<header class='mobile'>
+				<img class='logo' src='images/logo.png' width='60' height='auto'>
+				<h1>B-boy Rey-S</h1>
+			</header>
 
-		<div class='container-content-extra'>
-			<h1>I'm sorry</h1>
-			<h2>The page you're trying to visit doesn't exist. Please check the URL or try using the navigation.</h2>
-		</div>
-	</section>
+			<div class='container-content-extra'>
+				<h1>I'm sorry</h1>
+				<h2>The page you're trying to visit doesn't exist. Please check the URL or try using the navigation.</h2>
+			</div>
+		</section>
 
-<?php include 'includes/footer.php' ?>
+<?php include 'includes/footer.php'; ?>
 
-	<script src='scripts/script.js'></script>
-</body>
+		<script src='scripts/script.js'></script>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -1,111 +1,107 @@
 <?php
 	$pageTitle = 'B-Boy Rey-S | Home';
-	$robots = null;
-	$htmlClasses = null;
-	include 'includes/header.php' ?>
+	$styleAdd =
+		"<!-- Owl Carousel -->
+		<link rel='stylesheet' href='scripts/owl-carousel/owl.carousel.css'>
+		<link rel='stylesheet' href='scripts/owl-carousel/owl.theme.css'>";
+	include 'includes/header.php';
+?>
+		<section class='home'>
+			<header class='mobile'>
+				<img class='logo' src='images/logo.png' width='60' height='auto'>
+				<h1>B-boy Rey-S</h1>
+			</header>
 
-	<!-- Owl Carousel -->
-	<link rel='stylesheet' href='scripts/owl-carousel/owl.carousel.css'>
-	<link rel='stylesheet' href='scripts/owl-carousel/owl.theme.css'>
+			<div class='owl-carousel owl-theme'>
+				<div class='owl-slide'><img src='images/home-1.jpg'></div>
+				<div class='owl-slide'><img src='images/home-2.jpg'></div>
+				<div class='owl-slide'><img src='images/home-3.jpg'></div>
+				<div class='owl-slide'><img src='images/home-4.jpg'></div>
+			</div>
+		</section>
 
-<?php include 'includes/nav-desktop.php' ?>
+<?php include 'includes/footer.php'; ?>
 
-	<section class='home'>
-		<header class='mobile'>
-			<img class='logo' src='images/logo.png' width='60' height='auto'>
-			<h1>B-boy Rey-S</h1>
-		</header>
+		<!-- Owl Carousel -->
+		<script src='scripts/owl-carousel/owl.carousel.js'></script>
+		<script>
+			$(document).ready(function() {
+				var time = 8; // time in seconds
 
-		<div class='owl-carousel owl-theme'>
-			<div class='owl-slide'><img src='images/home-1.jpg'></div>
-			<div class='owl-slide'><img src='images/home-2.jpg'></div>
-			<div class='owl-slide'><img src='images/home-3.jpg'></div>
-			<div class='owl-slide'><img src='images/home-4.jpg'></div>
-		</div>
-	</section>
-
-<?php include 'includes/footer.php' ?>
-
-	<!-- Owl Carousel -->
-	<script src='scripts/owl-carousel/owl.carousel.js'></script>
-	<script>
-		$(document).ready(function() {
-			var time = 8; // time in seconds
-
-			var $progressBar,
-			$bar, 
-			$elem, 
-			isPause, 
-			tick,
-			percentTime;
-	
-			//Init the carousel
-			$('.owl-carousel').owlCarousel({
-				slideSpeed : 1000,
-				paginationSpeed : 500,
-				singleItem : true,
-				afterInit : progressBar,
-				afterMove : moved,
-				startDragging : pauseOnDragging
-			});
-	
-			//Init progressBar where elem is $('.owl-carousel')
-			function progressBar(elem){
-				$elem = elem;
-				//build progress bar elements
-				buildProgressBar();
-				//start counting
-				start();
-			}
-	
-			//create div#progressBar and div#bar then prepend to $('.owl-carousel')
-			function buildProgressBar(){
-				$progressBar = $('<div>',{
-					id:'progressBar'
+				var $progressBar,
+				$bar, 
+				$elem, 
+				isPause, 
+				tick,
+				percentTime;
+		
+				//Init the carousel
+				$('.owl-carousel').owlCarousel({
+					slideSpeed : 1000,
+					paginationSpeed : 500,
+					singleItem : true,
+					afterInit : progressBar,
+					afterMove : moved,
+					startDragging : pauseOnDragging
 				});
-				$bar = $('<div>',{
-					id:'bar'
-				});
-				$progressBar.append($bar).appendTo($elem);
-			}
-	
-			function start() {
-				//reset timer
-				percentTime = 0;
-				isPause = false;
-				//run interval every 0.01 second
-				tick = setInterval(interval, 10);
-			}
-	
-			function interval() {
-				if(isPause === false){
-					percentTime += 1 / time;
-					$bar.css({
-						width: percentTime+'%'
+		
+				//Init progressBar where elem is $('.owl-carousel')
+				function progressBar(elem){
+					$elem = elem;
+					//build progress bar elements
+					buildProgressBar();
+					//start counting
+					start();
+				}
+		
+				//create div#progressBar and div#bar then prepend to $('.owl-carousel')
+				function buildProgressBar(){
+					$progressBar = $('<div>',{
+						id:'progressBar'
 					});
-					//if percentTime is equal or greater than 100
-					if(percentTime >= 100){
-						//slide to next item 
-						$elem.trigger('owl.next')
+					$bar = $('<div>',{
+						id:'bar'
+					});
+					$progressBar.append($bar).appendTo($elem);
+				}
+		
+				function start() {
+					//reset timer
+					percentTime = 0;
+					isPause = false;
+					//run interval every 0.01 second
+					tick = setInterval(interval, 10);
+				}
+		
+				function interval() {
+					if(isPause === false){
+						percentTime += 1 / time;
+						$bar.css({
+							width: percentTime+'%'
+						});
+						//if percentTime is equal or greater than 100
+						if(percentTime >= 100){
+							//slide to next item 
+							$elem.trigger('owl.next')
+						}
 					}
 				}
-			}
-	
-			//pause while dragging 
-			function pauseOnDragging(){
-				isPause = true;
-			}
-	
-			//moved callback
-			function moved(){
-				//clear interval
-				clearTimeout(tick);
-				//start again
-				start();
-			}
-		});
-	</script>
+		
+				//pause while dragging 
+				function pauseOnDragging(){
+					isPause = true;
+				}
+		
+				//moved callback
+				function moved(){
+					//clear interval
+					clearTimeout(tick);
+					//start again
+					start();
+				}
+			});
+		</script>
 
-	<script src='scripts/script.js'></script>
-</body>
+		<script src='scripts/script.js'></script>
+	</body>
 </html>
